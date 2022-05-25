01c68bece016a8bd2c2c747241ebad9259749dee
Berk Topcu <b.topcu@emakina.com.tr>
2022-05-25 04:35:51 +0300

    initial commit and project setup

---
+++ src/test/java/com/berk2s/docman/DocmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berk2s.docman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
